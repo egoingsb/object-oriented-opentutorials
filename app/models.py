dea1c650df47696b78b9c916a05fe2f30857d07f
egoing <egoing@gmail.com>
2020-02-14 09:58:52 +0900

    관리자 활성화

--- app/models.py
+++ app/models.py
@@ -4,10 +4,13 @@ class Genre(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     pub_date = models.DateTimeField('publish date')
-    
+    def __str__(self):
+        return self.title
+
 class Topic(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     pub_date = models.DateTimeField('publish date')
     genre = models.ManyToManyField(Genre)
-
+    def __str__(self):
+        return self.title
