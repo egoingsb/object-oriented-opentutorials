d46d22c5f69f592f947fc025f47271ba4835b7f7
egoing <egoing@gmail.com>
2020-02-14 09:52:50 +0900

    implement model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+class Genre(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    pub_date = models.DateTimeField('publish date')
+    
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    pub_date = models.DateTimeField('publish date')
+    genre = models.ManyToManyField(Genre)
 
-# Create your models here.
