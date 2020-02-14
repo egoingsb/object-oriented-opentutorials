3c0374779bc6d09eba3e89b9339d0ffc75ddc4e3
egoing <egoing@gmail.com>
2020-02-14 10:13:51 +0900

    템플릿화

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hi')
+    return render(request, 'app/index.html', {
+        'contents':Topic.objects.all()
+    })
 
